fe4c62db0978ae2ea6d091dddc93591ec106f912
Hayden Stainsby <hds@caffeineconcepts.com>
2023-05-26 22:53:07 +0200

    prep: hello world async/await vs. futures implementation
    I found an even simpler async fucntion, hello world!

---
+++ resources/understanding-async-await/src/bin/hello.rs
@@ -0,0 +1,43 @@
+#[tokio::main]
+async fn main() {
+    // Hello, World async function
+    async_await::hello("world").await;
+    manual_future::hello("world").await;
+}
+
+mod async_await {
+    pub async fn hello(name: &'static str) {
+        println!("hello, {name}!");
+    }
+}
+
+mod manual_future {
+    use std::{future::Future, task::Poll};
+
+    pub fn hello(name: &'static str) -> Hello {
+        Hello::Init { name }
+    }
+
+    #[derive(Debug)]
+    pub enum Hello {
+        Init { name: &'static str },
+        Done,
+    }
+
+    impl Future for Hello {
+        type Output = ();
+
+        fn poll(
+            mut self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> Poll<Self::Output> {
+            match *self {
+                Hello::Init { name } => println!("hello, {name}!"),
+                Hello::Done => panic!("Please stop polling me!"),
+            };
+
+            *self = Hello::Done;
+            Poll::Ready(())
+        }
+    }
+}
